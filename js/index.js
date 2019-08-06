ac5b2f6f338b60e6a73a95723799b07800ff7d67
Jonathan Ho <johnnyho82@gmail.com>
2019-08-06 16:48:31 -0400

    Add mouseenter, mouseleave, click, dblclick, mouseover, keydown events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+const mainNav = document.querySelector('.main-navigation');
+mainNav.style.zIndex = '999';
+mainNav.addEventListener(
+  'mouseenter',
+  () => (mainNav.style.backgroundColor = '#FDBF52'),
+);
+mainNav.addEventListener(
+  'mouseleave',
+  () => (mainNav.style.backgroundColor = 'white'),
+);
+
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener(
+  'click',
+  () => (logoHeading.style.fontSize = '60px'),
+);
+
+const images = document.querySelectorAll('img');
+images.forEach(img => {
+  img.addEventListener('dblclick', () => {
+    img.style.transform = 'scaleX(-1)';
+    img.style.zIndex = '-999';
+  });
+});
+
+const footerTxt = document.querySelector('.footer p');
+footerTxt.addEventListener('mouseover', () => {
+  footerTxt.style.fontSize = '24px';
+  footerTxt.style.fontWeight = 'bold';
+  footerTxt.textContent = footerTxt.textContent
+    .split('')
+    .sort()
+    .join('');
+});
+
+const letsGoTxt = document.querySelector('.content-section .text-content h2');
+document.addEventListener(
+  'keydown',
+  e => (letsGoTxt.textContent = `${e.code}!`),
+);
