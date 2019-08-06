9a0d0c7f7795ba9f14b325dcce4d2393b19b3e0c
Jonathan Ho <johnnyho82@gmail.com>
2019-08-06 17:56:41 -0400

    Add wheel event

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // Your code goes here
+
+// Events 1 & 2: mouseenter & mouseleave
 const mainNav = document.querySelector('.main-navigation');
 mainNav.style.zIndex = '999';
 mainNav.addEventListener(
@@ -10,12 +12,14 @@ mainNav.addEventListener(
   () => (mainNav.style.backgroundColor = 'white'),
 );
 
+// Event 3: click
 const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener(
   'click',
   () => (logoHeading.style.fontSize = '60px'),
 );
 
+// Event 4: dblclick
 const images = document.querySelectorAll('img');
 images.forEach(img => {
   img.addEventListener('dblclick', () => {
@@ -24,6 +28,7 @@ images.forEach(img => {
   });
 });
 
+// Event 5: mouseover
 const footerTxt = document.querySelector('.footer p');
 footerTxt.addEventListener('mouseover', () => {
   footerTxt.style.fontSize = '24px';
@@ -34,23 +39,33 @@ footerTxt.addEventListener('mouseover', () => {
     .join('');
 });
 
+// Event 6: keydown
 const letsGoTxt = document.querySelector('.content-section .text-content h2');
 document.addEventListener(
   'keydown',
-  e => (letsGoTxt.textContent = `${e.code}!`),
+  e => (letsGoTxt.textContent = `Let's Go to ${e.code}!`),
 );
 
+// Event 7: resize
 const price = document.createElement('div');
 const welcomeTxt = document.querySelector('.intro h2');
 window.addEventListener(
   'resize',
   () =>
-    (welcomeTxt.textContent = `Welcome To Fun bus! Only $${window.innerWidth /
+    (welcomeTxt.textContent = `Welcome To Fun Bus! Only $${window.innerWidth /
       10}0 for a ticket!`),
 );
 
+// Event 8 & 9: drag & dragend
 const destHeader = document.querySelector('.content-destination h2');
 destHeader.draggable = 'true';
-document.addEventListener('drag', () => {
-  destHeader.style.fontSize = '18px';
+document.addEventListener('drag', () => (destHeader.style.fontSize = '18px'));
+document.addEventListener(
+  'dragend',
+  () => (destHeader.style.fontSize = '48px'),
+);
+
+// Event 10: wheel
+window.addEventListener('wheel', event => {
+  mainNav.style.borderBottom = `${Math.abs(event.deltaY)}px dashed black`;
 });
