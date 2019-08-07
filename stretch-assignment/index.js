a391867b33fd0b33fcfbbc312046d24f2ef7c2c8
Jonathan Ho <johnnyho82@gmail.com>
2019-08-07 08:51:53 -0400

    Use flexbox for stretch assignment

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,27 +1,36 @@
-const blockArr = Array.from(document.querySelectorAll('.block'));
+const blocks = document.querySelectorAll('.block');
 
-blockArr.forEach(block => {
-  let counter = 0;
-  const order = setInterval(() => {
-    counter += 1;
-    block.addEventListener('click', () => (block.style.order = -1 * counter));
-  }, 10);
-
-  block.addEventListener('mousedown', event => {
-    if (event.clientY < 175) {
-      let moveRight = 0;
-      block.style.position = 'relative';
-      const travel = setInterval(() => {
-        moveRight += 5;
-        block.style.left = `${moveRight}px`;
-      }, 10);
-    }
+blocks.forEach(block => {
+  block.addEventListener('click', () => {
+    blocks.forEach(block => block.style.order++);
+    block.style.order = '-5';
   });
-  console.log(blockArr);
+
+  const takeOff = TweenMax.to(block, 3, { x: 1000, paused: true });
+  block.addEventListener('mousedown', () => takeOff.play());
+  window.addEventListener('mouseup', () => takeOff.reverse());
 });
 
+// // Using setInterval
+
+// const blockArr = Array.from(document.querySelectorAll('.block'));
+
 // blockArr.forEach(block => {
-//   const action = TweenMax.to(block, 3, { x: 1000, paused: true });
-//   block.addEventListener('mousedown', () => action.play());
-//   window.addEventListener('mouseup', () => action.reverse());
+//   let counter = 0;
+//   const order = setInterval(() => {
+//     counter += 1;
+//     block.addEventListener('click', () => (block.style.order = -1 * counter));
+//   }, 10);
+
+//   block.addEventListener('mousedown', event => {
+//     if (event.clientY < 175) {
+//       let moveRight = 0;
+//       block.style.position = 'relative';
+//       const travel = setInterval(() => {
+//         moveRight += 5;
+//         block.style.left = `${moveRight}px`;
+//       }, 10);
+//     }
+//   });
+//   console.log(blockArr);
 // });
