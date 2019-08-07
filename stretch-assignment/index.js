12dc52ad6c41dd50c8b23ca8f43ce02d7f61eba9
Jonathan Ho <johnnyho82@gmail.com>
2019-08-06 23:08:16 -0400

    Finish MVP of stretch assignment

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,19 +1,24 @@
-const blocks = document.querySelector('.blocks');
+// const blocks = document.querySelector('.blocks');
 const blockArr = Array.from(document.querySelectorAll('.block'));
-const red = document.querySelector('.block--red');
-const blue = document.querySelector('.block--blue');
-const green = document.querySelector('.block--green');
-const pink = document.querySelector('.block--pink');
-const gray = document.querySelector('.block--gray');
+// const red = document.querySelector('.block--red');
+// const blue = document.querySelector('.block--blue');
+// const green = document.querySelector('.block--green');
+// const pink = document.querySelector('.block--pink');
+// const gray = document.querySelector('.block--gray');
 
-blockArr.forEach(block => {
-  //   block.addEventListener('click', () => {});
-  block.addEventListener('mousedown', () => {
-    let counter = 0;
+blockArr.forEach((block, index) => {
+  let counter = 0;
+  const order = setInterval(() => {
+    counter += 1;
+    block.addEventListener('click', () => (block.style.order = -1 * counter));
+  }, 10);
+
+  block.addEventListener('dblclick', () => {
+    let moveRight = 0;
     block.style.position = 'relative';
     const travel = setInterval(() => {
-      counter += 5;
-      block.style.left = `${counter}px`;
+      moveRight += 5;
+      block.style.left = `${moveRight}px`;
     }, 10);
   });
 });
